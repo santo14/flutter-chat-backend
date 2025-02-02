78bc80bfa1c1b1354e7947d59a3849a65b823a89
Santiago Velasquez <fvleasquez1425@gmail.com>
2025-02-02 02:27:14 -0500

    Fin sección 10

--- models/usuario.js
+++ models/usuario.js
@@ -24,7 +24,7 @@ const UsuarioSchema = Schema({
 });
 
 UsuarioSchema.method('toJSON', function() {
-    const {__v, _id, password, online, ...object} = this.toObject();
+    const {__v, _id, password, ...object} = this.toObject();
     object.uid = _id;
     return object;
 })
--- sockets/socket.js
+++ sockets/socket.js
@@ -1,14 +1,24 @@
+const { comprobarJWT } = require('../helpers/jwt');
 const { io } = require('../index');
-
+const {usuarioConectado, usuarioDesconectado, grabarMensaje} = require('../controllers/socket');
 
 // Mensajes de Sockets
 io.on('connection', client => {
     console.log('Cliente conectado');
+    const [valido, uid] = comprobarJWT(client.handshake.headers['x-token']);
 
-    client.on('disconnect', () => {
-        console.log('Cliente desconectado');
+    if (!valido) { return client.disconnect();}
+    
+    usuarioConectado(uid);
+
+    client.join(uid);
+
+    client.on('mensaje-personal', async (payload) => {
+        await grabarMensaje(payload);
+        io.to(payload.para).emit('mensaje-personal', payload);
     });
 
-    
-
+    client.on('disconnect', () => {
+        usuarioDesconectado(uid);
+    });
 });
